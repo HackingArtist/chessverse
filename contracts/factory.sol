233464b20e1be87ef350e7d8826ebd4bbf9371e3
Aashish <42600054+HackingArtist@users.noreply.github.com>
2021-12-10 22:18:20 +0530

    Update factory.sol

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -11,5 +11,16 @@ contract ChessFactory
         Escrow game=new Escrow(_userA, _amount);
         games.push(game);
         address(game);
+        IERC20 token=IERC20(0xa36085F69e2889c224210F603D836748e7dC0088);
+        address to=address(games[0]);
+        uint256 amount=1;
+
+        transferERC20(token, to, amount);
     }
+    
+     function transferERC20(IERC20 token, address to, uint256 amount) public{
+       
+       token.transfer(to,amount);
+
+    } 
 }
